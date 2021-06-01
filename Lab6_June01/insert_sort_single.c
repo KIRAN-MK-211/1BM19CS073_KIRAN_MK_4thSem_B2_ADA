3285ccea9b96ed9311e4c828cde817cdf58b167e
KIRAN-MK-211 <67577151+KIRAN-MK-211@users.noreply.github.com>
2021-06-01 15:09:11 +0530

    Update insert_sort_single.c

--- Lab6_June01/insert_sort_single.c
+++ Lab6_June01/insert_sort_single.c
@@ -1,18 +1,17 @@
-// C program for insertion sort
 #include <math.h>
 #include <stdio.h>
- 
-/* Function to sort an array using insertion sort*/
+#include <stdlib.h>
+#include <time.h>
+clock_t start,end;
+double time_taken;
+
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
- 
-        /* Move elements of arr[0..i-1], that are
-          greater than key, to one position ahead
-          of their current position */
+
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
@@ -21,7 +20,7 @@ void insertionSort(int arr[], int n)
     }
 }
  
-// A utility function to print an array of size n
+
 void printArray(int arr[], int n)
 {
     int i;
@@ -30,14 +29,22 @@ void printArray(int arr[], int n)
     printf("\n");
 }
  
-/* Driver program to test insertion sort */
+
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
- 
+    int arr[10],n;
+    printf("Enter no of elements\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    start = clock();
     insertionSort(arr, n);
+    end = clock();
+    time_taken = (double)(end - start)/CLOCKS_PER_SEC;
+    printf("Sorted array :: ");
     printArray(arr, n);
+    printf("\nNo of elements: %d||Time taken: %f\n",n,time_taken);
  
     return 0;
 }
